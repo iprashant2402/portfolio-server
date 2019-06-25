399eea02cc68d88c61ca2837551a14a100c28674
Prashant Singh <iprashant2402@gmail.com>
2019-06-25 13:10:53 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import {parseString} from 'xml2js';
+const PORT = process.env.PRIMARY_PORT;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/posts/', (req, res) => {
+    axios.get('http://medium.com/feed/@oldirony').then(function(response){    
+        parseString(response.data,(err,res2)=>{
+            res.send(JSON.stringify(res2.rss.channel[0].item));
+        })
+    }).catch(function(error){
+        console.log(error);
+    });
+  });
+
+app.listen(PORT,() => {
+    console.log("Listening on port : " + PORT);
+});
