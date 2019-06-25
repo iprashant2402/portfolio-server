d1cb8293f3d0d10efbe97f254b66d5800cfdd16b
Prashant Singh <iprashant2402@gmail.com>
2019-06-25 14:22:12 +0530

    Fixed Bugs

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 import {parseString} from 'xml2js';
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || '3000';
 
 const app = express();
 
 app.use(cors());
 
-app.get('/posts/', (req, res) => {
-    axios.get('https://medium.com/feed/@oldirony').then(function(response){    
+app.get('/', (req,res) => {
+    res.redirect('/posts');
+});
+
+app.get('/posts', (req, res) => {
+    axios.get('http://medium.com/feed/@oldirony').then(function(response){    
         parseString(response.data,(err,res2)=>{
             res.send(JSON.stringify(res2.rss.channel[0].item));
         })
